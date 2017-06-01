c68fe60deb63db73d500322fcf8f38a7cbbf86f7
Ben Smith <benjaminsmith1981@gmail.com>
2017-06-01 18:00:55 +0200

    OMDB Networking example

--- PhotoRama-Ch20/AppDelegate.swift
+++ PhotoRama-Ch20/AppDelegate.swift
@@ -11,10 +11,8 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         var keys: NSDictionary?
 
